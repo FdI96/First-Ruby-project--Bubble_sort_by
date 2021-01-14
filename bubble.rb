094927f59cbdfa9e714f8cd42aafe5933ac7727a
FdI96 <federicolopezce@gmail.com>
2021-01-14 19:29:38 -0300

    fixing linters

--- bubble.rb
+++ bubble.rb
@@ -1,20 +1,20 @@
-def bubble_sort_by
+def bubble_sort_by(a)
   b = false
-    while b
-      b = false
-      self.length().times do |i|
-        if self[i] != self.last && self[i] > self[i+1]
-          v = self[i];
-          self[i] = self[i+1];
-          self[i+1] = v;
-          b = true
-        end
-        i+=1;
+  while b
+    b = false
+    a.length().times do |i|
+      if a[i] != a.last && a[i] > a[i+1]
+        v = a[i];
+        a[i] = a[i+1]
+        a[i+1] = v
+        b = true
       end
+    i+=1
     end
-    p self
+  end
+  return a
 end
 
 array = [9,8,7,6,5,4,3,2,1,0];
-array.buuble_sort_by;
+buuble_sort_by(array);
 
